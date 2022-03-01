5e4244a760e19266a3178d62d621201b40eab043
ChiaraDellicarri <mc.dellicarri@elogic.it>
2022-03-01 17:55:34 +0100

    .

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import './main.css';
-
+import './js/fontawesome/brands.min.js';
+import './js/fontawesome/solid.min.js';
+import './js/fontawesome/fontawesome.min.js';
+import 'jquery.js';
 
 function importAll(r) {
     let images = {};
