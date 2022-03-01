f04d85a3922cf31faf3d77d5f1c5093fd3fc8cc6
ChiaraDellicarri <mc.dellicarri@elogic.it>
2022-03-01 18:32:23 +0100

    test jquery

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './main.css';
 import './js/fontawesome/brands.min.js';
 import './js/fontawesome/solid.min.js';
 import './js/fontawesome/fontawesome.min.js';
-import '/node_modules/jquery/dist/jquery.js';
+import '../node_modules/jquery/dist/jquery.js';
 
 function importAll(r) {
     let images = {};
@@ -10,4 +10,4 @@ function importAll(r) {
     return images;
   }
   
-  const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
+const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
